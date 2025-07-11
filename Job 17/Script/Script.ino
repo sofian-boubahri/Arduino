75398fe7660feafb12046c1b4ccebc6c825fe4a8
S. <shoyafr@gmail.com>
2025-07-11 09:04:18 +0200

    Update Script.ino

--- Job 17/Script/Script.ino
+++ Job 17/Script/Script.ino
@@ -1,23 +1,23 @@
-#include <LiquidCrystal.h> // Librairie pour Ecran LCD
+#include <LiquidCrystal.h> 
 
-int heure_date[6] = {10,20,15,9,07,2025}; //seconde minute heure jour mois année
-int mod[6] = {60,60,24,31,12,3000}; // limite d incrementation
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // On initialise la librairie avec les pins ou l'ecran est branche
+int heure_date[6] = {10,20,15,9,07,2025}; 
+int mod[6] = {60,60,24,31,12,3000}; 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
 
 void setup() {
-  lcd.begin(16, 2); // On initialise l'ecran avec sa taille
+  lcd.begin(16, 2);
 }
 
 void loop() {
-  heure_date[0]++;//incrementation du temps
+  heure_date[0]++;
     for (int i = 0; i < 6 ; i++){
-      if (heure_date[i] == mod[i]) { // verification du dépassement de limite
+      if (heure_date[i] == mod[i]) { 
         if (i<5) {heure_date[i+1] ++;}
         heure_date[i] = 0;
       }
     }
-    lcd.setCursor(0,0); //On place le curseur sur la première ligne
-    for (int j = 3; j < 5; j++) { //affichage de la date
+    lcd.setCursor(0,0); 
+    for (int j = 3; j < 5; j++) { 
       if (heure_date[j]<10){
         lcd.print("0");
         lcd.print(heure_date[j]);
@@ -26,8 +26,8 @@ void loop() {
       lcd.print("/");    
     }
     lcd.print(heure_date[5]); 
-    lcd.setCursor(0,1); //On place le curseur sur la deuxième ligne
-    for (int j = 2; j >= 0; j--) { //affichage de l'heure 
+    lcd.setCursor(0,1);
+    for (int j = 2; j >= 0; j--) { 
       if (heure_date[j]<10)
       {
         lcd.print ("0");
