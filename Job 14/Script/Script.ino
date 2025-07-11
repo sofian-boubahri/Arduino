c1ec519aec59e401f0c919ebfc02f123623a5d15
S. <shoyafr@gmail.com>
2025-07-11 09:01:59 +0200

    Update Script.ino

--- Job 14/Script/Script.ino
+++ Job 14/Script/Script.ino
@@ -1,9 +1,8 @@
 #include <LiquidCrystal.h>
 
-// Brochage : RS, E, D4, D5, D6, D7
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-const int buttonPin = 7; // le bouton est connecté à la broche 7
+const int buttonPin = 7; 
 bool running = true;
 bool lastButtonState = HIGH;
 unsigned long lastDebounceTime = 0;
@@ -13,14 +12,13 @@ unsigned long previousMillis = 0;
 int seconds = 0;
 
 void setup() {
-  lcd.begin(16, 2);  // initialiser l'écran LCD 16 colonnes et 2 lignes
-  pinMode(buttonPin, INPUT_PULLUP); // bouton avec résistance interne
+  lcd.begin(16, 2); 
+  pinMode(buttonPin, INPUT_PULLUP);
   lcd.setCursor(0, 0);
   lcd.print("SoloQ");
 }
 
 void loop() {
-  // Lecture du bouton avec anti-rebond
   bool reading = digitalRead(buttonPin);
   if (reading != lastButtonState) {
     lastDebounceTime = millis();
@@ -28,19 +26,18 @@ void loop() {
 
   if ((millis() - lastDebounceTime) > debounceDelay) {
     if (reading == LOW && lastButtonState == HIGH) {
-      running = !running;  // Inverser l'état (pause / lecture)
+      running = !running;  
     }
   }
   lastButtonState = reading;
 
-  // Mise à jour du compteur si actif
   if (running && millis() - previousMillis >= 1000) {
     previousMillis = millis();
     seconds++;
 
-    lcd.setCursor(0, 1);  // ligne 2
+    lcd.setCursor(0, 1); 
     lcd.print("Temps: ");
     lcd.print(seconds);
-    lcd.print(" s     "); // espace pour effacer les anciens chiffres
+    lcd.print(" s     ");
   }
 }
